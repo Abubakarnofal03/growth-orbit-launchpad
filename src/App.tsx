67a1e5f7a6f624d3905aaa836869bc75644f4528
AbubakarNofal03 <abubakarnofal0786@gmail.com>
2025-11-14 01:54:20 +0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -26,18 +27,28 @@ const App = () => {
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          {isLoading && <LoadingScreen onComplete={handleLoadingComplete} />}
-          {!isLoading && (
-            <BrowserRouter>
-              <Routes>
-                <Route path="/" element={<Index />} />
-                <Route path="/case-study/:id" element={<CaseStudyDetail />} />
-                <Route path="/blog" element={<Blog />} />
-                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </BrowserRouter>
-          )}
+          <AnimatePresence mode="wait">
+            {isLoading ? (
+              <LoadingScreen key="loading" onComplete={handleLoadingComplete} />
+            ) : (
+              <motion.div
+                key="content"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.8, ease: [0.25, 0.1, 0.25, 1] }}
+              >
+                <BrowserRouter>
+                  <Routes>
+                    <Route path="/" element={<Index />} />
+                    <Route path="/case-study/:id" element={<CaseStudyDetail />} />
+                    <Route path="/blog" element={<Blog />} />
+                    {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                    <Route path="*" element={<NotFound />} />
+                  </Routes>
+                </BrowserRouter>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </TooltipProvider>
       </LanguageProvider>
     </QueryClientProvider>
